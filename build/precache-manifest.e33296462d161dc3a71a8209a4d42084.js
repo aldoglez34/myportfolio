a372fc40931b7e3228a1d684a1527a8af34b99c8
Aldo Solano González <aldoglez34@gmail.com>
2020-08-11 11:59:46 -0500

    msg

---
+++ build/precache-manifest.e33296462d161dc3a71a8209a4d42084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e51f76a728a36c9b8be9d86f338ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8c86ae9475210e8a11f",
+    "url": "/static/css/main.9d11793c.chunk.css"
+  },
+  {
+    "revision": "eed325fa7601ff33bb83",
+    "url": "/static/js/2.f8330768.chunk.js"
+  },
+  {
+    "revision": "d8c86ae9475210e8a11f",
+    "url": "/static/js/main.8df4bbd2.chunk.js"
+  },
+  {
+    "revision": "b1012eb6e955899a327d",
+    "url": "/static/js/runtime~main.78214684.js"
+  },
+  {
+    "revision": "61b7ed2fd32ef0cc20704815a9bf0b3e",
+    "url": "/static/media/bg-small.61b7ed2f.jpg"
+  },
+  {
+    "revision": "91aefff5479aef713c94cdd877c73d85",
+    "url": "/static/media/bg.91aefff5.jpg"
+  }
+]);
